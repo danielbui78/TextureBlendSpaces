b6cbca0fc5c51c2ba81bb040fdd847983cd77765
danielbui78 <danielbui78@gmail.com>
2023-07-26 01:33:13 -0700

    Initial commit

---
+++ Source/TextureBlendSpaces/TextureBlendSpacesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "TextureBlendSpacesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEXTUREBLENDSPACES_API ATextureBlendSpacesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
